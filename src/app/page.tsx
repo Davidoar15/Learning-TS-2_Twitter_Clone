52ca2cdcde1f6c576419a1c3fe647cb05f6cdbf2
Davidoar15 <davidoar15@gmail.com>
2023-08-21 13:58:51 -0300

    local environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">

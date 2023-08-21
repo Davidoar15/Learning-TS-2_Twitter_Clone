60e0f1a4748d98a77276a67c96cc5562560fc80f
Davidoar15 <davidoar15@gmail.com>
2023-08-21 17:47:17 -0300

    RouteLogin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { AuthButtonServer } from "../components/authButton-server";
 export default function Login () {
     return (
         <section className='grid place-content-center min-h-screen'>
-            <h1>Start Session</h1>
+            <h1 className="text-xl font-bold mb-4">Start Session</h1>
             <AuthButtonServer />
         </section>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { AuthButtonServer } from "./components/authButton-server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
+  const { data: { session } } = await supabase.auth.getSession()
+  if (session === null) {
+    redirect('/login');
+  }
+  
   const { data: posts } = await supabase.from('posts').select("*");
 
   return (

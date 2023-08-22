bc69d74069dc401f31a1775b481500c23dc846be
Davidoar15 <davidoar15@gmail.com>
2023-08-21 23:07:43 -0300

    add/NextConfig

--- src/app/components/composePost.tsx
+++ src/app/components/composePost.tsx
@@ -1,18 +1,34 @@
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 
 export function ComposePost({
     userAvatarUrl
 } : {
     userAvatarUrl: string
 }) {
+
+    const addPost = async (formData: FormData) => {
+        'use server'
+
+        const content = formData.get('content');
+        if (content === null) return 
+
+        const supabase = createServerActionClient({ cookies });
+        const { data: { user }} = await supabase.auth.getUser();
+        if (user === null) return
+
+        await supabase.from('posts').insert({ content, user_id: user.id });
+
+        revalidatePath('/');
+    }
+
     return (
-        <form className="flex flex-row space-x-4 p-4 border-b border-white/20">
-            <img className="rounded-full w-10 h-10 object-contain" src={userAvatarUrl} />
+        <form action={addPost} className="flex flex-row p-3 border-b border-white/20">
+            <img className="rounded-full w-10 h-10 object-contain mr-4" src={userAvatarUrl} />
             <div className="flex flex-l flex-col gap-y-4">
                 <textarea
-                    name='post'
+                    name='content'
                     rows={4}
                     className="w-full text-2xl bg-black placeholder-gray-500 p-2"
                     placeholder="WHAT'S GOING ON!?"
